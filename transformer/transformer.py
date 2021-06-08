ce8ea6cbf90caf37553f80e783dc88267ab2e9a8
Vitaliy Aksenov <37418655+viviaxenov@users.noreply.github.com>
2021-06-08 18:28:41 +0300

    Fixed tabs

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -135,28 +135,27 @@ class Transformer(nn.Module):
         super().__init__()
 
         self.seq_len_image = seq_len_image
-	self.img_dim = int(np.sqrt(seq_len_image))
+		self.img_dim = int(np.sqrt(seq_len_image))
         self.seq_len_text  = seq_len_text
 
         total_tokens = text_tokens + img_tokens
 
         self.embedding = nn.Embedding(total_tokens, dim_model)
 	
-	# learned embedding for positional information
-	self.text_pos_embedding = nn.Embedding(seq_len_text, dim_model)
-	self.img_row_embedding = nn.Embedding(img_dim, dim_model)
-	self.img_col_embedding = nn.Embedding(img_dim, dim_model)
+		# learned embedding for positional information
+		self.text_pos_embedding = nn.Embedding(seq_len_text, dim_model)
+		self.img_row_embedding = nn.Embedding(img_dim, dim_model)
+		self.img_col_embedding = nn.Embedding(img_dim, dim_model)
 	
-	# labels of positions 
-	self.text_pos = torch.arange(seq_len_text, dtype=torch.int, device=src.device).reshape(1, -1, 1)
-	pos_matrix = torch.stack([torch_arange(img_dimg, dtype=torch.int, device=src.device)]*img_dim)
-	self.img_row_pos = pos_matrix.reshape(1, -1, 1)
-	self.img_col_pos = pos_matrix.t.reshape(1, -1, 1)
+		# labels of positions 
+		self.text_pos = torch.arange(seq_len_text, dtype=torch.int, device=src.device).reshape(1, -1, 1)
+		pos_matrix = torch.stack([torch_arange(img_dimg, dtype=torch.int, device=src.device)]*img_dim)
+		self.img_row_pos = pos_matrix.reshape(1, -1, 1)
+		self.img_col_pos = pos_matrix.t.reshape(1, -1, 1)
 	
         self.layers = nn.ModuleList([
             TransformerLayer(dim_model, num_heads, dim_feedforward, dropout)
-            for _ in range(num_layers)
-        ])
+            for _ in range(num_layers)])
 
         self.output = nn.Linear(dim_model, total_tokens)
         
@@ -167,7 +166,7 @@ class Transformer(nn.Module):
         
         # add positional data for text
         src[:, :self.seq_len_text] += self.text_pos_embedding(self.text_pos)
-	# add positional data for rows and columns of the image
+		# add positional data for rows and columns of the image
         src[:, self.seq_len_text:] += self.img_row_embedding(self.img_row_pos)
         src[:, self.seq_len_text:] += self.img_col_embedding(self.img_col_pos)
 
